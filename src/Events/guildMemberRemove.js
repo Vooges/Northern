92b033e1a5c896ee1af4fa4716ebc3bd8549f188
Vooges <jessevooges@gmail.com>
2021-09-26 23:48:07 +0200

    Added ban command and made some small changes

--- src/Events/guildMemberRemove.js
+++ src/Events/guildMemberRemove.js
@@ -3,7 +3,7 @@ const Event = require("../Structures/Event.js");
 const Discord = require("discord.js");
 
 module.exports= new Event("guildMemberRemove", (client, member) => {
-    const channel = member.guild.channels.cache.find(c => c.name == "welcome-or-goodbye");
+    const channel = member.guild.channels.cache.find(c => c.name == "welcome-and-goodbye");
 
     if(!channel) return;
 
