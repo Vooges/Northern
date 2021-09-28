2b3e81bd7fc8add7b2487b14457c6094d6ac5fac
Vincent Brouwer <dviide@protonmail.com>
2021-09-28 23:23:31 +0200

    switch to erela

---
+++ src/Events/raw.js
@@ -0,0 +1,5 @@
+const Event = require("../Structures/Event.js");
+
+module.exports = new Event("raw", (client, data) => {
+	client.manager.updateVoiceState(data);
+});
--- src/Events/ready.js
+++ src/Events/ready.js
@@ -2,10 +2,12 @@ const Event = require("../Structures/Event.js");
 const pjson = require("../../package.json");
 
 module.exports = new Event("ready", (client) => {
-    client.user.setPresence({
-        activities: [{ name: `version: ${pjson.version}-dev` }],
-        status: 'online',
-    });
+	client.user.setPresence({
+		activities: [{ name: `version: ${pjson.version}-dev` }],
+		status: "online",
+	});
 
-    console.log("Apollo is ready!");
+	client.manager.init(client.user.id);
+
+	console.log("Apollo is ready!");
 });
